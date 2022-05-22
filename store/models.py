742dc1cae82514275a2a140123eab36c051e6487
prateekb1912 <prateekb1912@gmail.com>
2022-05-22 15:20:01 +0530

    Add Address model and define a one-one relation with Customer

--- store/models.py
+++ store/models.py
@@ -40,3 +40,9 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_CHOICES, default=PAYMENT_PENDING)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    pincode = models.BigIntegerField(max_length=6)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
