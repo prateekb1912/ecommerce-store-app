81efe83c6b7fb29835360239d67870b6b419df19
prateekb1912 <prateekb1912@gmail.com>
2022-05-26 13:50:31 +0530

    Make changes in database models

--- store/models.py
+++ store/models.py
@@ -3,13 +3,15 @@ from django.db import models
 # Create your models here.
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 class Product(models.Model):
     title = models.CharField(max_length=256)
+    slug = models.SlugField(default='-')
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
-    last_updated = models.DateTimeField(auto_now_add = True)
+    last_update = models.DateTimeField(auto_now_add = True)
 
     collection = models.ForeignKey('Collection', on_delete=models.PROTECT)
 
@@ -24,8 +26,8 @@ class Customer(models.Model):
         (MEMBERSHIP_GOLD, 'Gold')
     ]
 
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=16)
     dob = models.DateField(null=True)
