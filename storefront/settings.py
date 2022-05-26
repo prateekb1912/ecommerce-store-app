b58bd37336a3759ae3f28d8d2d6f9ed0ffc9281c
prateekb1912 <prateekb1912@gmail.com>
2022-05-26 13:05:58 +0530

    Setup postgresql database

--- storefront/settings.py
+++ storefront/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront',
+        'USER': 'patrick',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'       
     }
 }
 
