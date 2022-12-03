b4bc6aa09b164a49015e2db7e4d8be09a257b329
prateekb1912 <prateekb1912@gmail.com>
2022-12-03 19:56:23 +0530

    change db pass

--- storefront/settings.py
+++ storefront/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'store',
         'USER': 'patrick',
-        'PASSWORD': '12345',
+        'PASSWORD': 'patrick19135',
         'HOST': 'localhost',
         'PORT': '5432'       
     }
